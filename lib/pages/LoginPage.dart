ee006ccaf97c5a2ccfc49729ee13b89966104764
BenZina <slaheddine.ben.zina@gmail.com>
2020-04-20 20:01:46 +0200

    adding textfield and button

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutterapptest/localizations/AppLocalizations.dart';
 import 'package:flutterapptest/services/ProfileServices.dart';
+import 'package:flutterapptest/constants.dart' as Constants;
+import 'package:flutterapptest/utils/SizeConfig.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -42,43 +45,79 @@ class _PageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            color: Colors.black,
-            child: AnimatedOpacity(
-              duration: Duration(milliseconds: 2000),
-              opacity: (showImage) ? 1.0 : 0.0,
-              child: Image.asset(
-                "assets/images/splash_back.jpeg",
-                fit: BoxFit.cover,
+      body: Container(
+        color:  Constants.backgroundColors,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.asset(
+                "assets/images/logo.png",
+                fit: BoxFit.fill,
+                height: SizeConfig.blockSizeVertical * 14,
+                width: SizeConfig.blockSizeHorizontal * 56,
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CircularProgressIndicator(
-                        valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 20.0),
-                      ),
-                    ],
+              SizedBox(height: 50,),
+              Container(
+                decoration: BoxDecoration(
+                  color: Constants.backgroundColorsBlur,
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                ),
+                height: SizeConfig.blockSizeVertical * 6,
+                width: SizeConfig.blockSizeHorizontal * 80,
+                child: TextFormField(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                        fontWeight: FontWeight.w300
+                    ),
+                    decoration: InputDecoration(
+                        hintStyle: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.w100),
+                        border: InputBorder.none,
+                        hintText: AppLocalizations.of(context).translate("email"),
+                    ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                decoration: BoxDecoration(
+                  color: Constants.backgroundColorsBlur,
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                ),
+                height: SizeConfig.blockSizeVertical * 6,
+                width: SizeConfig.blockSizeHorizontal * 80,
+                child: TextFormField(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300
                   ),
-                )
-              ],
-            ),
-          )
-        ],
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.w100),
+                    border: InputBorder.none,
+                    hintText: AppLocalizations.of(context).translate("password"),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              MaterialButton(
+                height: SizeConfig.blockSizeVertical * 5.5,
+                minWidth: SizeConfig.blockSizeHorizontal * 50,
+                color: Constants.secondColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(40.0),
+                ),
+                child: Text(AppLocalizations.of(context).translate("login").toUpperCase(),
+                    textAlign: TextAlign.center ,style: TextStyle(fontSize :17, fontWeight: FontWeight.normal, color: Colors.black,)),
+                onPressed: () {
+
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
